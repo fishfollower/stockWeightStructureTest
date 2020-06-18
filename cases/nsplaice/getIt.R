b43580a37956babe3014cf71333b608a4fcb3b66
casperwberg <casperwberg@gmail.com>
2020-06-18 13:09:52 +0200

    update nsplaice

--- cases/nsplaice/getIt.R
+++ cases/nsplaice/getIt.R
@@ -10,4 +10,9 @@ logN = t(fit$pl$logN)
 
 write.table(exp(logN),file="N.tab",row.names=FALSE,col.names=FALSE)
 
+matur = fit$data$propMat
+
+write.table(matur,file="Mo.tab",row.names=FALSE,col.names=FALSE)
+
+
  par(mfrow=n2mfrow(ncol(sw))); for(i in 1:ncol(sw)) plot(logN[,i],log(sw[,i]),main=i)
